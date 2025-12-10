60792b28f0cae682dd7c32b77396b55929734738
muhammad-tamim <contact2tamim@gmail.com>
2025-12-10 16:00:07 +0600

    feat: add validate Request middleware by zod

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodObject, ZodRawShape } from "zod";
+
+const validateRequest = (schema: ZodObject<ZodRawShape>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const parsed = schema.safeParse(req.body);
+
+        if (!parsed.success) {
+            return res.status(400).send({
+                success: false,
+                error: parsed.error.issues
+            })
+        }
+
+        req.body = parsed.data
+        next()
+    }
+}
+
+export default validateRequest
